a1a959f4c90d4a131170050f1eea01d10fafc377
AdilNaskenov <adil.n2006@icloud.com>
2026-02-02 00:14:24 +0500

    Adds OOP

--- Togizkumalak/Togizkumalak.cpp
+++ Togizkumalak/Togizkumalak.cpp
@@ -4,196 +4,265 @@
 #include <ctime>
 #include <cmath>
 #include <fstream>
-
-
 using namespace std;
 
-// Константы цветов
 const string RED = "\033[31m";
 const string GREEN = "\033[32m";
 const string RESET = "\033[0m";
 
-// Функция для отрисовки доски (чтобы не повторять код)
-void printBoard(int kazan[18], int score1, int score2) {
-    system("cls");
-    cout << "Ochco (PC): " << score2 << endl;
+class Player {
+private:
+	string name;
+	int rating;
 
-    // Верхний ряд (Игрок 2 / PC)
-    for (int i = 9; i >= 1; i--) cout << i << "\t";
-    cout << endl;
-    for (int i = 17; i >= 9; i--) cout << GREEN << kazan[i] << RESET << "\t";
-    cout << endl << endl;
+public:
+	Player() {
+		ifstream readName("name");
+		ifstream readElo("ELOhu");
+		if (readName.is_open() && readElo.is_open()) {
+			readElo >> rating;
+			getline(readName, name);
+			cout << "Welcom back!: " << name << "\nYour ELO: " << rating << endl;
+			readName.close();
+			readElo.close();
+		}
+		else {
+			cout << "Choose your name: ";
+			cin >> name;
+			rating = 400;
+			ofstream Name("name");
+			ofstream Elo("ELOhu");
+			if (Name.is_open() && Elo.is_open()) {
+				Name << name;
+				Elo << rating;
+			}
+			Name.close();
+			Name.close();
+		}
+	}
+};
 
-    // Нижний ряд (Игрок 1 / Ты)
-    for (int i = 0; i < 9; i++) cout << GREEN << kazan[i] << RESET << "\t";
-    cout << endl;
-    for (int i = 1; i <= 9; i++) cout << i << "\t";
-    cout << endl;
+class Board {
+private:
+	int pits[18];
+	int kazan1 = 0;
+	int kazan2 = 0;
+	
+public:
+	Board() {
+		for (int i = 0; i < 18; i++) pits[i] = 9;
+	}
+	bool isPitEmpty(int player, int choice) {
+		int index = (player == 1) ? (choice - 1) : (choice - 1 + 9);
+		return pits[index] == 0;
+	}
+	// Тот самый метод!
+	void makeMove(int choice) {
+		int startPit = choice - 1; // Переводим ввод игрока (1-9) в индекс (0-8)
+		int hand = pits[startPit];
 
-    cout << "Ochco (You): " << score1 << endl << "--------------------------" << endl;
-}
+		if (hand == 0) {
+			std::cout << "Oshibka! Lunka pusta." << std::endl;
+			return;
+		}
 
+		// Правило Тогыз Кумалак:
+		if (hand == 1) {
+			pits[startPit] = 0;
+			int nextPit = (startPit + 1) % 18;
+			pits[nextPit]++;
+		}
+		else pits[startPit] = 1; // Один оставляем
+		int stonesToDistribute = hand - 1;
+
+		int currentPit = (startPit + 1) % 18;
+
+		for (int i = 0; i < stonesToDistribute; i++) {
+			pits[currentPit]++;
+			currentPit = (currentPit + 1) % 18; // Вот так мы ходим по кругу бесконечно
+		}
+		if (currentPit <= 17 && currentPit >= 9) {
+			if (pits[currentPit] % 2 == 0) {
+				kazan1 += pits[currentPit];
+				pits[currentPit] = 0;
+				std::cout << "!!! Zahvat! Vi zabrali kamni protivnika !!!" << std::endl;
+			}
+		}
+		
+		// После хода всегда показываем результат
+		show();
+
+	}
+
+	void makeMovePC(int choice) {
+		// 1. Вычисляем индекс (для PC это 9-17)
+		int startPit = (choice - 1) + 9;
+		int hand = pits[startPit];
+
+		if (hand == 0) return; // Компьютер просто не должен выбирать пустые лунки
+
+		int currentPit; // Объявляем снаружи, чтобы видеть её после цикла
+
+		if (hand == 1) {
+			pits[startPit] = 0;
+			currentPit = (startPit + 1) % 18;
+			pits[currentPit]++;
+		}
+		else pits[startPit] = 1;
+
+		int stonesToDistribute = hand - 1;
+		currentPit = (startPit + 1) % 18;
+
+		for (int i = 0; i < stonesToDistribute; i++) {
+			pits[currentPit]++;
+			currentPit = (currentPit + 1) % 18;
+		}
+		// 2. Логика захвата для PC (на стороне игрока 0-8)
+		if (currentPit >= 0 && currentPit <= 8) {
+			if (pits[currentPit] % 2 == 0) {
+				kazan2 += pits[currentPit]; // В КАЗАН КОМПЬЮТЕРА
+				pits[currentPit] = 0;
+				std::cout << "PC zabral vashi kamni!" << std::endl;
+			}
+		}
+		show();
+	}
+
+	void show() {
+		cout << "Ochco (PC): " << kazan2 << endl;
+
+		for (int i = 9; i >= 1; i--) cout << i << "\t";
+		cout << endl;
+
+
+
+		// Она видит pits напрямую, не нужно его передавать!
+		int temp[9];
+
+		for (int i = 17; i >= 9; i--) {
+			int a = -1 * (i - 17);
+			temp[a] = pits[i];
+		}
+		while (true) {
+			int error = 0;
+			for (int i = 0; i < 9; i++) {
+				if (temp[i] == 0) {
+					error++;
+					cout << " \t";
+				}
+				else if (temp[i] == 1) {
+					cout << "*\t";
+					temp[i] = 0;
+					error++;
+				}
+				else {
+					cout << "**\t";
+					temp[i] -= 2;
+				}
+			}
+			if (error == 9) break;
+			cout << endl;
+		}
+		cout << endl << endl;
+		// Доска
+		int sum = 0;
+		for (int i = 0; i < 9; i++) {
+			temp[i] = pits[i];
+			if (temp[i] > sum) sum = temp[i];
+		}
+		int end = (sum++) / 2;
+
+		for (int j = 0; j <= end; j++) {
+			int sum = 0;
+			for (int i = 0; i < 9; i++) {
+				if (temp[i] > sum) sum = temp[i];
+			}
+
+			if (sum % 2 == 0) sum--;
+
+			for (int i = 0; i < 9; i++) {
+				if (temp[i] == 0) cout << " \t";
+				else if (temp[i] >= sum) {
+					if (temp[i] % 2 == 0) {
+						cout << "**\t";
+						temp[i] -= 2;
+					}
+					else if (temp[i] % 2 == 1) {
+						cout << "*\t";
+						temp[i]--;
+					}
+				}
+				else cout << " \t";
+			}
+			cout << endl;
+		}
+
+
+		cout << endl;
+
+
+
+		for (int i = 1; i <= 9; i++) cout << i << "\t";
+		cout << endl;
+
+		cout << "Ochco (You): " << kazan1 << endl << "--------------------------" << endl;
+	}
+};
+
+// 2. А здесь начинается сама игра
 int main() {
-    while (true) {
-        int first = 0, second = 0;
-        int kazan[18];
-        for (int i = 0; i < 18; i++) kazan[i] = 9; // Инициализация
+	// Создаем объекты (те самые "шаблоны" или "красные кнопки")
+	Player p1;
+	Board game;
+	
+	// Показываем начальную доску
+	game.show();
+	// ВОТ ЗДЕСЬ мы вызываем метод!
+	// Представь, что пользователь ввел число 3
+	bool hod = true; // true - ты, false - PC
+	while (true) {
+		int choice;
 
-        bool tyzgik_exists = true; // Можно ли еще создать туздык
-        int tslot = -1;
-        srand(time(0));
-        bool hod = true; // true - твой ход, false - PC
+		if (hod) {
+			std::cout << "--- Vash hod! ---" << std::endl;
+			std::cout << "Viberite lunku (1-9): ";
+			std::cin >> choice;
 
+			// Тут можно добавить проверку: если лунка пустая, попросить ввести еще раз
+			game.makeMove(choice);
 
+			hod = false; // Передаем ход компьютеру
+		}
+		else {
+			std::cout << "--- Hod PC... ---" << std::endl;
 
+			// Логика выбора для PC (как мы обсуждали раньше)
+			int pcChoice;
+			do {
+				pcChoice = (rand() % 9) + 1;
+			} while (game.isPitEmpty(2, pcChoice)); // Тебе нужно добавить этот метод в класс
 
-        while (true) {
-            printBoard(kazan, first, second);
-            int n;
-            // Логика выбора лунки
-            if (hod) {
-                do { n = 1 + rand() % 9; } while (kazan[n - 1] == 0);
-             /*   do { cin >> n; } while (kazan[n - 1] == 0);*/
-                cout << "You choose: " << n << endl;
-            }
-            else {
-                // Ход PC
-                int lunka[9];  
-                for (int j = 0; j < 9; j++) lunka[j] = 0;  
+			game.makeMovePC(pcChoice);
 
-                for (int i = 10; i <= 18; i++) {
-                    int a = (kazan[i - 1] + i - 1) - 18;
-                    if (kazan[a - 1] > 0 && (kazan[a - 1] + 2) % 3 == 0) {
-                        lunka[i - 10] = kazan[a - 1];
-                    }
-                }
+			hod = true; // Возвращаем ход тебе
+		}
+		
+	}
 
-                int more = 0;
-                for (int i = 1; i <= 9; i++) {
-                    if (lunka[i - 1] > more) {
-                        more = lunka[i - 1];
-                    }
-                }
-
-                do { n = 10 + rand() % 9; } while (kazan[n - 1] == 0);
-
-                for (int i = 0; i <= 8; i++) {
-                    if (kazan[i] == more) {  
-                        n = i - 1;
-                    }
-                }
-               
-                cout << "PC chooses: " << n << endl;
-            }
-
-            int index = n - 1;
-            int balls = kazan[index];
-           
-
-            kazan[index] = (balls == 1) ? 0 : 1; // Если 1 шарик, он уходит в след. лунку. Если больше - один остается.
-            int balls_to_distribute = (balls == 1) ? 1 : balls - 1;
-
-            int step = index + 1;
-            int last_pos = index;
-
-            for (int i = 0; i < balls_to_distribute; i++) {
-                if (step > 17) step = 0;
-                kazan[step]++;
-                last_pos = step;
-                step++;
-            }
-
-            // Правила захвата (Четность)
-            if (hod && last_pos >= 9 && last_pos <= 17) {
-                if (kazan[last_pos] % 2 == 0) {
-                    first += kazan[last_pos];
-                    kazan[last_pos] = 0;
-                }
-                else if (tyzgik_exists && kazan[last_pos] == 3 && last_pos != 17) {
-                    // Туздык (нельзя в последней лунке)
-                    tslot = last_pos;
-                    tyzgik_exists = false;
-                    first += 3;
-                    kazan[last_pos] = 0;
-                }
-            }
-            else if (!hod && last_pos >= 0 && last_pos <= 8) {
-                if (kazan[last_pos] % 2 == 0) {
-                    second += kazan[last_pos];
-                    kazan[last_pos] = 0;
-                }
-                else if (tyzgik_exists && kazan[last_pos] == 3 && last_pos != 8) {
-                    tslot = last_pos;
-                    tyzgik_exists = false;
-                    second += 3;
-                    kazan[last_pos] = 0;
-                }
-            }
-
-            // Сбор из Туздыка (если он есть)
-            if (tslot != -1) {
-                if (tslot >= 9 && tslot <= 17) { first += kazan[tslot]; kazan[tslot] = 0; }
-                else { second += kazan[tslot]; kazan[tslot] = 0; }
-            }
-
-            // Проверка победы
-            if (first > 81 || second > 81) break;
-
-            hod = !hod;
-            /*cin.get();*/ // Пауза до нажатия Enter
-        }
-
-
-        // Посчет ЭЛО
-        string ELOhu, ELOpc;
-
-        ifstream readELOhu("ELOhu"), readELOpc("ELOpc");
-        if (readELOhu.is_open() && readELOpc.is_open()) {
-            getline(readELOhu, ELOhu);
-            getline(readELOpc, ELOpc);
-            readELOhu.close();
-            readELOpc.close();
-        }
-        else {
-            ofstream ELOhu1("ELOhu"), ELOpc1("ELOpc");
-            if (ELOhu1.is_open() && ELOpc1.is_open()) {
-                ELOhu1 << 400;
-                ELOpc1 << 400;
-                ELOhu1.close();
-                ELOpc1.close();
-            }
-        }
-
-
-        int ratingHu = stoi(ELOhu);
-        int ratingPc = stoi(ELOpc);
-
-        double S, S1;
-        if (first > 81) { S = 1; S1 = 0; }
-        else if (second > 81) { S = 0; S1 = 1; }
-        else S = 0.5;
-        // рейтинг человека
-        double E = 1.0 / (1.0 + pow(10.0, (double)(ratingPc - ratingHu) / 400.0));
-        int R = ratingHu + 20 * (S - E);
-
-        // рейтинг компа
-        double E1 = 1.0 / (1.0 + pow(10.0, (double)(ratingHu - ratingPc) / 400.0));
-        int R1 = ratingPc + 20 * (S1 - E1);
-
-        ofstream FileELOhu("ELOhu"), FileELOpc("ELOpc");
-        if (FileELOhu.is_open() && FileELOpc.is_open()) {
-            FileELOhu << R;
-            FileELOpc << R1;
-            FileELOpc.close();
-            FileELOhu.close();
-        }
-
-        cout << (first > 81 ? "YOU WIN!" : "PC WINS!") << endl;
-        cout << "Human rating: " << R << endl << "PC rating: " << R1 << endl;
-        cin.ignore(1000, '\n');
-    }
-    return 0;
+	return 0;
 }
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
